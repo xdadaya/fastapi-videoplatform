221832684c8825d016122540d678f1410e7660f2
xdadaya <gerakrah@gmail.com>
2023-03-28 10:38:55 +0300

    removed optional

--- statistics_service/app/services/message_model.py
+++ statistics_service/app/services/message_model.py
@@ -5,10 +5,10 @@ from pydantic import BaseModel
 
 class MessageBody(BaseModel):
     user_id: str
-    videos_amount: Optional[int] = 0
-    comments_amount: Optional[int] = 0
-    total_text_length: Optional[int] = 0
-    total_rating: Optional[int] = 0
+    videos_amount: int = 0
+    comments_amount: int = 0
+    total_text_length: int = 0
+    total_rating: int = 0
 
 
 class MessageScheme(BaseModel):
